0df006928cafe45a6b4b3be13a6b2d2611ff78c8
aorupert <aor11@georgetown.edu>
2022-04-14 14:14:40 -0400

    final version

--- app/alphavantage_service.py
+++ app/alphavantage_service.py
@@ -23,3 +23,12 @@ def fetch_crypto(symbol):
 
     tsd = parsed_response["Time Series (Digital Currency Daily)"]
     return tsd
+
+#def fetch_crypto(symbol):
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
+def fetch_stocks(symbol):
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}&datatype=csv"
+    response = requests.get(url)
+    parsed_response = json.loads(response.text)
+    tsd = parsed_response["Some stock words"]
+    return tsd
--- app/stocks.py
+++ app/stocks.py
@@ -1,23 +1,30 @@
 
 
 print("STOCKS REPORT...")
+from app.alphavantage_service import fetch_stocks
+
 
 import os
 from dotenv import load_dotenv
+import pandas
 from pandas import read_csv
 
-load_dotenv()
+#load_dotenv()
 
-ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
+#ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
 
 symbol = input("Please input a crypto symbol (default: 'NFLX'): ") or "NFLX"
-url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}&datatype=csv"
+#url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}&datatype=csv"
+tsd = fetch_stocks(symbol)
 
 df = read_csv(url)
-#print(df.columns)
+#maybe bring this back
+#i moved this to alpha file
+print(df.columns)
 #breakpoint()
 
 latest = df.iloc[0]
+#same with this
 
 print(symbol)
 print(latest["timestamp"])
